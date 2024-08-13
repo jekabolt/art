9dec1ce3ca1f781cc773c2b07a2e70a69f26d498
jekabolt <jekabolt@yahoo.com>
2024-08-13 18:13:35 +0200

    wip

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/script.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.glsl$/,
+                use: 'glslify-loader',
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/assets', to: 'assets' } // Copies everything from public/assets to dist/assets
+            ],
+        }),
+    ],
+    mode: 'production',
+};
